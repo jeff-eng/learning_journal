34b9375b6cfb51c207f4cad6e748e2291b479f4b
jeff-eng <me@jeffeng.com>
2023-11-07 18:13:40 -0800

    Moved functions to own file to be shared amongst blogpost.js, index.js, and about.js

--- js/blogpost.js
+++ js/blogpost.js
@@ -1,9 +1,5 @@
 import { blogPosts } from './data.js';
-import { createBasicElement } from './functions.js';
-
-const htmlElementTable = {
-    article: 'article'
-};
+import { createPostItem } from './functions.js';
 
 const reverseChronoPosts = blogPosts.sort((postA, postB) => {
     const dateA = new Date(postA.publishedDate);
@@ -15,26 +11,3 @@ const reverseChronoPosts = blogPosts.sort((postA, postB) => {
 document.getElementById('feature-post-container').innerHTML = reverseChronoPosts.slice(0)[0].renderedHtml;
 // Render recent post previews
 document.getElementById('recent-posts').append(...reverseChronoPosts.slice(1, 4).map((post) => createPostItem(post)));
-  
-function createPostItem(blogPost) {
-    const article = createBasicElement(htmlElementTable.article, `post-item-${blogPost.id}`, 'blog-post');
-
-    article.innerHTML = 
-        `<a href="#" aria-label="go to article: ${blogPost.title}">                        
-            <picture class="blog-post__image">
-                <source media="(min-width: )" srcset="${blogPost.webpFilepath}" type="image/webp">
-                <img src="${blogPost.jpgFilepath}" alt="${blogPost.imgDescription}">
-            </picture>
-        </a>
-        <div class="blog-post-wrapper">
-            <header class="blog-post__header">
-                <time class="blog-post__date" datetime="YYYY-MM-DD">${blogPost.getFormattedDateString()}</time>
-                <a href="#" class="blog-post__link">
-                    <h2 class="blog-post__title">${blogPost.title}</h2>
-                </a>
-            </header>
-            ${blogPost.getFirstParagraph()}
-        </div>`;
-
-    return article;
-}
--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,7 @@
+const htmlElementTable = {
+    article: 'article'
+};
+
 function createBasicElement(tagType, idName='', ...classNames) {
     const htmlElement = document.createElement(tagType);
 
@@ -7,4 +11,27 @@ function createBasicElement(tagType, idName='', ...classNames) {
     return htmlElement;
 }
 
-export { createBasicElement }
+function createPostItem(blogPost) {
+    const article = createBasicElement(htmlElementTable.article, `post-item-${blogPost.id}`, 'blog-post');
+
+    article.innerHTML = 
+        `<a href="#" aria-label="go to article: ${blogPost.title}">                        
+            <picture class="blog-post__image">
+                <source media="(min-width: )" srcset="${blogPost.webpFilepath}" type="image/webp">
+                <img src="${blogPost.jpgFilepath}" alt="${blogPost.imgDescription}">
+            </picture>
+        </a>
+        <div class="blog-post-wrapper">
+            <header class="blog-post__header">
+                <time class="blog-post__date" datetime="YYYY-MM-DD">${blogPost.getFormattedDateString()}</time>
+                <a href="#" class="blog-post__link">
+                    <h2 class="blog-post__title">${blogPost.title}</h2>
+                </a>
+            </header>
+            ${blogPost.getFirstParagraph()}
+        </div>`;
+
+    return article;
+}
+
+export { createPostItem }
