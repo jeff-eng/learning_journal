9f71236341843a5c08291f52709e309762b7c1e0
jeff-eng <me@jeffeng.com>
2023-11-27 16:23:09 -0800

    Commented out blogPost function that creates new dummy blog posts (for now).

--- js/functions.js
+++ js/functions.js
@@ -63,4 +63,52 @@ function setYear() {
     });
 }
 
+// function BlogPost() {
+//     this.id = crypto.randomUUID();
+//     this.publishedDate = '2023-06-01';
+//     this.webpFilepath = '';
+//     this.jpgFilepath  = 'https://picsum.photos/309/232';
+//     this.imgDescription = 'placeholder image';
+//     this.postHtmlString: `<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+//                          ut labore et dolore magna aliqua.</p>
+//                         <picture>
+//                             <source media="" srcset="/images/article_5.webp" type="image/webp">
+//                             <img src="/images/article_5.jpg" alt="laptop on desk viewed at angle">
+//                         </picture>
+//                         <h2>Litora Feugiat Eu Maximus Erat Facilisis Torquent</h2>
+//                         <p>Nunc sed id semper risus in hendrerit gravida. Platea dictumst vestibulum rhoncus est pellentesque 
+//                         elit ullamcorper.</p>
+//                         <p>Ut tortor pretium viverra suspendisse potenti nullam ac. In fermentum posuere urna nec tincidunt 
+//                         praesent semper feugiat nibh.</p>
+//                         <h2>Rutrum Massa Lacus Torquent Justo At</h2>
+//                         <p>Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Ac felis donec et odio pellentesque.</p>`,
+//         get renderedHtml() { 
+//             return this.renderArticleHtml(
+//             this.getFormattedDateString(), 
+//             this.title, 
+//             this.postHtmlString
+//             )
+//         },
+//         renderArticleHtml: function(dateString, title, postHtml) {
+//             return `<article class="feature-post">
+//                         <header class="feature-post__header">
+//                             <time class="feature-post__date" datetime="YYYY-MM-DD">${dateString}</time>
+//                             <h1 class="feature-post__heading">${title}</h1>
+//                         </header>
+//                         ${postHtml}
+//                     </article>`;
+//         },
+//         getFirstParagraphText: function() {
+//             const parser = new DOMParser();
+//             // Create HTMLDocument from string
+//             const htmlDoc = parser.parseFromString(this.postHtmlString, 'text/html');
+//             return htmlDoc.querySelector('p').textContent;
+//         },
+//         getFormattedDateString: function() {
+//             const d = new Date(this.publishedDate);
+//             const options = { timeZone: 'UTC', year: 'numeric', month: 'long', day: 'numeric' };
+//             return d.toLocaleDateString('en-US', options);
+//         }
+// }
+
 export { createHeroPostItem, createPostItem, setYear };
