cbda976c3845871694e998e0d35f8af2311b7474
jeff-eng <me@jeffeng.com>
2024-02-04 20:24:57 -0800

    Added function to create a dummy post and used Post class to create new Post instances.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,19 @@
 import { blogPosts } from './data.js';
-import { createHeroPostItem, createPostItem, setYear } from './functions.js';
+import { createHeroPostItem, createPostItem, setYear, generateRandomUID, getFormattedRandomDate } from './functions.js';
 import { Post } from './Post.js';
 
-const reverseChronoPosts = blogPosts.sort((postA, postB) => {
+const titles = ['Scrimba Rocks', 
+                'It Takes Practice', 
+                'Learn with Repetition', 
+                'Do Code Reviews',
+                'Write. Refactor. Repeat.',
+                'Think marathon. Not Sprint.',
+                'Stay the course'
+               ];
+const reverseChronoPosts = blogPosts.map(postData => {
+    const { id, title, publishedDate, webpFilepath, jpgFilepath, imgDescription, postHtmlString } = postData;
+    return new Post(id, title, publishedDate, webpFilepath, jpgFilepath, imgDescription, postHtmlString);
+}).sort((postA, postB) => {
     const dateA = new Date(postA.publishedDate);
     const dateB = new Date(postB.publishedDate);
     return dateB - dateA;
@@ -13,7 +24,7 @@ const loadPostsQty = 3;
 
 // Infinite scroll - auto-load posts when scrolling to bottom of page
 window.onscroll = () => {
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 10) {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 75) {
         loadPosts();
     }
 };
@@ -37,13 +48,44 @@ function loadPosts() {
         document.getElementById('recent-posts').append(...postsToDisplay.map((post) => createPostItem(post)));
 
         for (let i = 0; i < loadPostsQty; i++) {
-            const postObj = new Post();
-
+            const postObj = createDummyPost(titles);
             reverseChronoPosts.push(postObj);
         }
     }
-
 }
+
+function createDummyPost(titles) {
+    const dummyPostData = {
+        randomUID: generateRandomUID(25),
+        title: titles[Math.floor(Math.random() * titles.length)],
+        publishedDate: getFormattedRandomDate((new Date(2023, 0, 1)), (new Date())),
+        webpFilePath: Post.generateImageURL(false),
+        jpgFilePath: Post.generateImageURL(true),
+        imgDescription: 'placeholder image',
+        postHtmlString: `<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                         ut labore et dolore magna aliqua.</p>
+                         <picture>
+                            <source media="" srcset="https://picsum.photos/309/232.webp" type="image/webp">
+                            <img src="https://picsum.photos/309/232.jpg" alt="placeholder">
+                         </picture>
+                         <h2>Litora Feugiat Eu Maximus Erat Facilisis Torquent</h2>
+                         <p>Nunc sed id semper risus in hendrerit gravida. Platea dictumst vestibulum rhoncus est pellentesque 
+                         elit ullamcorper.</p>
+                         <p>Ut tortor pretium viverra suspendisse potenti nullam ac. In fermentum posuere urna nec tincidunt 
+                         praesent semper feugiat nibh.</p>
+                         <h2>Rutrum Massa Lacus Torquent Justo At</h2>
+                         <p>Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Ac felis donec et odio pellentesque.</p>` 
+    };
+
+    return new Post(dummyPostData.randomUID, 
+                    dummyPostData.title,
+                    dummyPostData.publishedDate,
+                    dummyPostData.webpFilePath,
+                    dummyPostData.jpgFilePath,
+                    dummyPostData.imgDescription,
+                    dummyPostData.postHtmlString
+                   );
+} 
 
 // Load initial set of posts
 loadPosts();
